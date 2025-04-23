a4d40489e797d7c5d2b94766f22541053883fa31
Nour Hamdan <nour.ham96@gmail.com>
2025-04-23 11:36:32 +0300

    final updates.

--- main.py
+++ main.py
@@ -11,14 +11,33 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
-
-
-if option == "Temperature":
-    filtered_data = [item["main"]["temp"] for item in filtered_data]
-    figure = px.line(x= dates, y= temperatures, labels = {"x": "Date", "y": "Temperature (C)"})
-    st.plotly_chart(figure)
+if place:
     
-if option == "Sky":
-    filtered_data = [item["weather"][0]["main"] for item in filtered_data]
-    st.image()
+    try:
+        filtered_data = get_data(place, days)
+
+
+        if option == "Temperature":
+            temperatures = [item["main"]["temp"] / 10 for item in filtered_data]
+            dates = [item["dt_txt"] for item in filtered_data]
+            
+            figure = px.line(x= dates, y= temperatures, labels = {"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+            
+        if option == "Sky":
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"
+            }
+            
+            sky_conditions = [item["weather"][0]["main"] for item in filtered_data]
+            # We got a dictionary mapping the weather condition with the proper image
+            # Now to connect (translate):
+            image_paths = [images[condition] for condition in sky_conditions]
+            
+            st.image(image_paths, width=115)
+            
+    except KeyError:
+        st.write("That place does not exist.")
